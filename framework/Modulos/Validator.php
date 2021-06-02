8d0e172187ad8da79837f8816f4522261be104e2
Alberto Garcia <alberto.garcia@zataca.com>
2021-06-02 11:21:40 +0200

    Refactor validator

--- framework/Modulos/Validator.php
+++ framework/Modulos/Validator.php
@@ -2,9 +2,62 @@
 namespace AlbertoCore\Modulos;
 
 class Validator{
-
-    public function __invoke(){
-        
+    public $response = [
+        'valid'=>true,
+    ];
+    public function __invoke($rule,$key,$input){
+        call_user_func_array([$this,$rule],[$key,$input]);
+        return $this->response;
     }
 
+
+    public function isRequired($campo,$input){
+        if (!empty($input[$campo])){
+            return true;
+        } 
+        $this->response['valid']=false;
+        $this->response[$campo]='Este campo no puede estar vacío.';      
+        return false;
+    }
+    public function isString($campo,$input){
+        if (is_string($input[$campo]) || empty($input[$campo]) ){
+            return true;
+        }
+        $this->response['valid']=false;
+        $this->response[$campo]='Este campo debe ser una cadena de texto.';
+        return false;
+
+    }
+    public function isNumeric($campo,$input){
+        if (is_numeric($input[$campo]) || empty($input[$campo])){
+            return true;
+        }
+        $this->response['valid']=false;
+        $this->response[$campo]='Este campo debe ser un numero.';
+        return false;
+    }
+    public function isAlphaNumeric($campo,$input){
+        if (ctype_alnum($input[$campo]) || empty($input[$campo])) {
+            return true;
+        }
+        $this->response['valid']=false;
+        $this->response[$campo]='Este campo no permite espacios ni caracteres especiales.';
+        return false;
+    }
+    public function isEmail($campo,$input){
+        if (filter_var($input[$campo],FILTER_VALIDATE_EMAIL) || empty($input[$campo])) {
+            return true;
+        }
+        $this->response['valid']=false;
+        $this->response[$campo]='El Email no es valido';
+        return false;
+    }
+    public function onlyLetters($campo,$input){
+        if (ctype_alpha($input[$campo]) || empty($input[$campo])) {
+            return true;
+        }
+        $this->response['valid']=false;
+        $this->response[$campo]='Este campo solo permite letras';
+        return false;
+    }
 }
--- src/Utils/Application/ValidarRequestCommand.php
+++ src/Utils/Application/ValidarRequestCommand.php
@@ -1,66 +1,22 @@
 <?php
 namespace Src\Utils\Application;
+use AlbertoCore\Modulos\Validator;
 
 class ValidarRequestCommand{
-    public $response = [
-        'valid'=>true,
-    ];
+    public $validator;
+    public $validation;
 
-    public function isRequired($campo,$input){
-        if (!empty($input[$campo])){
-            return true;
-        } 
-        $this->response['valid']=false;
-        $this->response[$campo]='Este campo no puede estar vacío.';      
-        return false;
+    public function __construct(Validator $validator)
+    {
+        $this->validator = $validator;
     }
-    public function isString($campo,$input){
-        if (is_string($input[$campo]) || empty($input[$campo]) ){
-            return true;
-        }
-        $this->response['valid']=false;
-        $this->response[$campo]='Este campo debe ser una cadena de texto.';
-        return false;
 
-    }
-    public function isNumeric($campo,$input){
-        if (is_numeric($input[$campo]) || empty($input[$campo])){
-            return true;
-        }
-        $this->response['valid']=false;
-        $this->response[$campo]='Este campo debe ser un numero.';
-        return false;
-    }
-    public function isAlphaNumeric($campo,$input){
-        if (ctype_alnum($input[$campo]) || empty($input[$campo])) {
-            return true;
-        }
-        $this->response['valid']=false;
-        $this->response[$campo]='Este campo no permite espacios ni caracteres especiales.';
-        return false;
-    }
-    public function isEmail($campo,$input){
-        if (filter_var($input[$campo],FILTER_VALIDATE_EMAIL) || empty($input[$campo])) {
-            return true;
-        }
-        $this->response['valid']=false;
-        $this->response[$campo]='El Email no es valido';
-        return false;
-    }
-    public function onlyLetters($campo,$input){
-        if (ctype_alpha($input[$campo]) || empty($input[$campo])) {
-            return true;
-        }
-        $this->response['valid']=false;
-        $this->response[$campo]='Este campo solo permite letras';
-        return false;
-    }
     public function __invoke($input, array $contrato){
         foreach ($contrato as $key =>$value) {
-            foreach ($value as $campo) { 
-                call_user_func_array([$this,$campo],[$key,$input]);
+            foreach ($value as $rule) { 
+                $this->validation = ($this->validator)($rule,$key,$input);
             }
         }
-        return $this->response;
+        return $this->validation;
     }
 }
